6de7e5dfa9ec83c06b2d4aa86d2527f2534bee3c
davidivad96 <davidivad96@gmail.com>
2021-04-08 11:55:33 +0200

    Created basic Blockchain class

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -1,1 +1,19 @@
 # Module 1 - Create a Blockchain
+
+# To be installed:
+# Flask==1.1.2
+# Postman HTTP Client
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+
+# Part 2 - Mining our Blockchain
